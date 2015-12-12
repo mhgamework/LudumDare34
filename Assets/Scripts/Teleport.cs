d6ef9de7ae0d22a4ba702b856f0178614d95a06b
Michiel Huygen <mhgamework@gmail.com>
2015-12-12 19:03:16 +0100

    Implemented teleporter

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -1,28 +1,90 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Teleport : MonoBehaviour
 {
-
     public Teleport Target;
     public bool TeleportingActive = true;
 
-    // Use this for initialization
-    void Start()
-    {
+    public float ChargeTime = 2;
 
+    private float chargeAmount = 0;
+
+    public MeshRenderer meshRenderer;
+
+    private Color beamStartColor;
+    // Use this for initialization
+    private void Start()
+    {
+        beamStartColor = meshRenderer.materials[1].color;
+        //StartCoroutine(blip().GetEnumerator());
     }
 
     // Update is called once per frame
     void Update()
     {
+        meshRenderer.materials[1].color = Color.Lerp(beamStartColor, Color.red, chargeAmount / ChargeTime);
 
+
+    }
+
+    public void FixedUpdate()
+    {
+        chargeAmount = Mathf.Clamp(chargeAmount - Time.deltaTime*2, 0, ChargeTime + 0.001f);
+    }
+
+    public IEnumerable<YieldInstruction> blip()
+    {
+        for (;;)
+        {
+            /*var betweenBlip = EasingFunctions.Ease(EasingFunctions.TYPE.In, chargeAmount / ChargeTime, 1, 0.1f);
+
+            while (betweenBlip > 0)
+            {
+                betweenBlip -= Time.deltaTime;
+                meshRenderer.materials[1].color = Color.Lerp(beamStartColor, Color.red, betweenBlip);
+                yield return null;
+            }*/
+
+        }
+        return null;
     }
 
     public void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         if (Target != null)
-            Gizmos.DrawLine(transform.position + Vector3.up*0.5f, Target.transform.position + Vector3.up * 0.5f);
+            Gizmos.DrawLine(transform.position + Vector3.up * 0.5f, Target.transform.position + Vector3.up * 0.5f);
+
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        var tp = other.GetComponent<Teleportable>();
+        if (!tp) return;
+        if (tp.InTeleporter) return;
+        //tp.InTeleporter = true;
+        //tp.TeleportTo(Target.transform.position);
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        var tp = other.GetComponent<Teleportable>();
+        if (!tp) return;
+        //chargeAmount = 0;
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        var tp = other.GetComponent<Teleportable>();
+        if (!tp) return;
+        chargeAmount += Time.deltaTime*3; // Times two since we decrement at every fixedupdate *2
+
+
+
+        if (chargeAmount > ChargeTime)
+            tp.TeleportTo(Target.transform.position);
     }
 }
---
+++ Assets/Scripts/Teleportable.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleportable : MonoBehaviour
+{
+
+    public Transform TeleportTransform;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public bool InTeleporter     { get; set; }
+
+    public void TeleportTo(Vector3 pos)
+    {
+        TeleportTransform.position = pos;
+
+    }
+}
