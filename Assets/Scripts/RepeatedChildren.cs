5f33599ff01d9b5cba9e294e272e27b07cc2a84b
Michiel Huygen <mhgamework@gmail.com>
2015-12-12 23:36:06 +0100

    Added epic moveable bridges

---
+++ Assets/Scripts/RepeatedChildren.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+[ExecuteInEditMode]
+public class RepeatedChildren : MonoBehaviour
+{
+
+    public int NumRepeat = 4;
+    public GameObject Prefab;
+    public Vector3 RelativeOffset = new Vector3(0, 0, 1);
+    public Vector3 BaseOffset = new Vector3(0, 0, 0);
+
+    private int lastHash;
+
+    void Awake()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.isPlaying) return;
+        if (!transform.Cast<Transform>().FirstOrDefault(c => c.name.StartsWith ( "[GEN]")))
+        {
+            createChildren();
+            return;
+        }
+        if (lastHash == GetHashCode()) return;
+        lastHash = GetHashCode();
+        createChildren();
+    }
+
+    private void createChildren()
+    {
+        Debug.Log("Generating children");
+        foreach (var c in transform.Cast<Transform>().Where(c => c.name.StartsWith("[GEN]")).ToArray())
+        {
+            DestroyImmediate(c.gameObject);
+        }
+        for (int i = 0; i < NumRepeat; i++)
+        {
+            var child = Instantiate(Prefab);
+            child.name = "[GEN] " + child.name;
+            child.transform.SetParent(transform);
+            child.transform.localPosition = BaseOffset + RelativeOffset * i;
+        }
+    }
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            int hashCode = base.GetHashCode();
+            hashCode = (hashCode*397) ^ NumRepeat;
+            hashCode = (hashCode*397) ^ (Prefab != null ? Prefab.GetHashCode() : 0);
+            hashCode = (hashCode*397) ^ RelativeOffset.GetHashCode();
+            hashCode = (hashCode*397) ^ BaseOffset.GetHashCode();
+            return hashCode;
+        }
+    }
+}
---
+++ Assets/Scripts/SwitchableStairsScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class SwitchableStairsScript : MonoBehaviour
+{
+
+    public float RelativeMinY= -2;
+    public float RelativeMaxY = 2;
+    public float RelativeY = 0;
+    public float RelativeTargetY = 0;
+    public float SwitchSpeed = 1;
+
+    public bool AnchorLeft = true;
+
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    var diff =  RelativeTargetY- RelativeY;
+	    diff = Mathf.Sign(diff)*Mathf.Min(Mathf.Abs( diff), Time.deltaTime*SwitchSpeed);
+
+        RelativeY = Mathf.Clamp(RelativeY + diff   , RelativeMinY, RelativeMaxY);
+
+
+	    //var startAngle = Mathf.Atan2(transform.position.z, transform.position.x);
+
+	    //var angleLength = Length/new Vector2(transform.position.x, transform.position.z).magnitude;
+
+	    int i = 0;
+        foreach (var child in transform.Cast<Transform>())
+	    {
+	        var p = child.position;
+
+	        //var angle = Mathf.Atan2(p.z, p.x);
+	        var factor = 1f/ (transform.childCount+1) * (i+1);//(angle - startAngle)/angleLength;
+	        if (!AnchorLeft) factor = 1 - factor;//angleLength - factor;
+            p.y = Mathf.Lerp(0, RelativeY, factor);
+	        p.y += +transform.position.y;
+
+            child.position = p ;
+	        i++;
+	    }
+	}
+
+    public void Toggle()
+    {
+        if (RelativeTargetY == RelativeMinY) RelativeTargetY = RelativeMaxY;
+        else if (RelativeTargetY == RelativeMaxY) RelativeTargetY = RelativeMinY;
+        else RelativeTargetY = RelativeMinY;
+
+    }
+}
