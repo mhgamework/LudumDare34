d879b26091e0d71de89a5a1bc4805efb4f380ec6
MHGW-Laptop <mhgamework@gmail.com>
2015-12-12 17:51:53 +0100

    Added colors, textures. Added pressureplate script.

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+public class PressurePlate : MonoBehaviour
+{
+    [Serializable]
+    public class OnPressurePlateActivatedEventHandler : UnityEvent { }
+    [Serializable]
+    public class OnPressurePlateDeactivatedEventHandler : UnityEvent { }
+
+    public OnPressurePlateActivatedEventHandler OnPressurePlateActivated;
+    public OnPressurePlateDeactivatedEventHandler OnPressurePlateDeactivated;
+
+    void Start()
+    {
+        InitialMeshHeight = MeshTransform.localPosition.y;
+    }
+
+    void Update()
+    {
+        if (CurrentColliders.Count != PrevColliderCount)
+        {
+            if (CurrentColliders.Count == 0)
+                DeactivatePressurePlate();
+            else
+                ActivatePressurePlate();
+
+            PrevColliderCount = CurrentColliders.Count;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PressurePlateActivator>() != null)
+        {
+            CurrentColliders.Add(other);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        CurrentColliders.Remove(other);
+    }
+
+    private void ActivatePressurePlate()
+    {
+        if (OnPressurePlateActivated != null)
+            OnPressurePlateActivated.Invoke();
+
+        var pos = MeshTransform.localPosition;
+        MeshTransform.localPosition = new Vector3(pos.x, InitialMeshHeight - 0.1f, pos.z);
+    }
+
+    private void DeactivatePressurePlate()
+    {
+        if (OnPressurePlateDeactivated != null)
+            OnPressurePlateDeactivated.Invoke();
+
+        var pos = MeshTransform.localPosition;
+        MeshTransform.localPosition = new Vector3(pos.x, InitialMeshHeight, pos.z);
+    }
+
+    private List<Collider> CurrentColliders = new List<Collider>();
+    private int PrevColliderCount;
+
+    [SerializeField]
+    private Transform MeshTransform = null;
+
+    private float InitialMeshHeight;
+}
---
+++ Assets/Scripts/PressurePlateActivator.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePlateActivator : MonoBehaviour
+{
+    //Doesnt do anything, just a tag to be recognized by the pressureplte.
+}
