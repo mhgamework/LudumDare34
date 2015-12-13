958dac3bd3559290c2d5b716980458e832e325aa
Michiel Huygen <mhgamework@gmail.com>
2015-12-13 18:21:16 +0100

    Girlfriend is now muy smarter

--- Assets/RobotGirlController.cs
+++ Assets/RobotGirlController.cs
@@ -25,14 +25,21 @@ public class RobotGirlController : MonoBehaviour
         var diff = TargetHeight - CurrentHeight;
         CurrentHeight +=Mathf.Sign(diff)* Mathf.Min( Mathf.Abs( diff), Time.deltaTime * MoveSpeed);
 
-        IsMoving = Math.Abs(TargetHeight - transform.position.y) > 0.001;
+        IsMoving = Math.Abs(TargetHeight - CurrentHeight) > 0.001;
 
         var radius = new Vector2(transform.position.x, transform.position.z).magnitude;
 
-        var angle = CurrentHeight * AngleScale + AngleOffset; ;
+        var angle = CurrentHeight * AngleScale + AngleOffset; 
 
         var pos = new Vector3(Mathf.Cos(angle) * radius, CurrentHeight, Mathf.Sin(angle) * radius);
+        var targetPos = new Vector3(Mathf.Cos(angle)*(radius + 0.1f), CurrentHeight, Mathf.Sin(angle)*(radius + 0.1f)) + PositionOffset;
         transform.position = pos + PositionOffset;
+
+
+
+        transform.LookAt(targetPos);
+        transform.Rotate(Vector3.up, YRotation);
+
     }
 
     [SerializeField]
@@ -47,4 +54,6 @@ public class RobotGirlController : MonoBehaviour
 
     public Vector3 PositionOffset;
     public float MoveSpeed = 1;
+
+    public float YRotation;
 }
