89b2e66ded3bf0530ea1b289fc95511612f23734
Simon Chuptys <simon.chuptys@gmail.com>
2018-04-12 22:41:18 +0200

    First playable version!

---
+++ Assets/VR/SyncVRCameraToPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncVRCameraToPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private Transform playerTransform = null;
+    [SerializeField]
+    private Vector3 offset = new Vector3();
+    
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, playerTransform.position.y, transform.position.z) + offset;
+    }
+}
